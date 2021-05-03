55544b86ec7a387d5bbf48c331e24227419b3302
Sharan Aithal <32029982+sharan-aithal@users.noreply.github.com>
2021-05-03 10:38:50 +0530

    check installation of ffmpeg

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -4,9 +4,16 @@
 
 parse_args() {
 while [ $# -gt 0 ]; do
+	# TODO: Parse arguments
 	echo "$@"
 	shift
 done
 }
 
+check_install() {
+path=$(command -v $1) || echo "$1 not in PATH variable"
+}
+
 parse_args $@
+
+check_install "ffmpeg"
