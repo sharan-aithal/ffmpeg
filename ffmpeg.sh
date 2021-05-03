2778099020561f1810f4da227481af50c3f666a3
Sharan Aithal <32029982+sharan-aithal@users.noreply.github.com>
2021-05-03 13:43:24 +0530

    add i/o validation

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -6,8 +6,28 @@ show_help() {
 echo "Usage: $0 "
 }
 
+do_ffmpeg() {
+	ffmpeg -i $1 $2
+}
+
+check_input_output() {
+	if [ "$1" == "" ]; then
+		echo "input file is required"
+	elif [ "$2" == "" ]; then
+		echo "output file is required"
+	elif [ $# -le 2 -a $# -gt 0 ]; then
+		do_ffmpeg $1 $2
+	else
+		echo "invalid arguments"
+	fi
+}
+
 parse_args() {
 ARGS=$@
+if [ $# -eq 0 ]; then
+	echo "No arguments passed"
+	show_help
+else
 while [ $# -gt 0 ]; do
 	# TODO: Parse arguments
 	case "$ARGS" in
@@ -15,18 +35,21 @@ while [ $# -gt 0 ]; do
 			show_help
 			;;
 		*)
-			echo "Invalid option"
+			check_input_output $@
+			shift
 			;;
 	esac
 	echo "$# $@" # debugging only
 	shift
 done
+fi
 }
 
 check_install() {
 path=$(command -v $1) || echo "$1 not in PATH variable"
 }
 
-parse_args $@
 
 check_install "ffmpeg"
+
+parse_args $@
